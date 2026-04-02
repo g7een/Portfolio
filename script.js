c9da6ed6752f7dde655ca7708fe22af25b8c2cf2
Amit <amityadlapati@gmail.com>
2026-04-02 09:53:09 -0500

    b

--- script.js
+++ script.js
@@ -457,8 +457,8 @@ const mouseInfo = document.getElementById("mouseInfo");
 
 
 document.addEventListener("mousemove", (e) => {
-    const x = (e.clientX / window.innerWidth - 0.5) * 20;
-    const y = (e.clientY / window.innerHeight - 0.5) * 20;
+    const x = (e.clientX/window.innerWidth - 0.5) * 20;
+    const y = (e.clientY/window.innerHeight - 0.5) * 20;
 
     items.forEach((item, i) => {
         const depth = (i + 1) * 0.3;
